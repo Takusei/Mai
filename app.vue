6f549902236d754dca3c704a4d78b013d35cd222
Jaycen <saitakusei@gmail.com>
2024-07-08 14:05:53 +0900

    Add vuetify and history

--- app.vue
+++ app.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-timeline align="start">
+    <v-timeline-item
+      v-for="(option, i) in travelOptions"
+      :key="i"
+      :dot-color="option.color"
+      size="small"
+    >
+      <template v-slot:opposite>
+        <div
+          :class="`pt-1 headline font-weight-bold text-${option.color}`"
+          v-text="option.year"
+        ></div>
+      </template>
+      <div>
+        <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${option.color}`">
+          {{  option.title }}
+        </h2>
+        <div>
+          {{  option.description }}
+        </div>
+      </div>
+    </v-timeline-item>
+  </v-timeline>
 </template>
+
+<script setup>
+import {travelOptions} from './utils/options.ts'
+
+const years = travelOptions
+</script>
---
+++ utils/options.ts
@@ -0,0 +1,130 @@
+const year2022 = [
+  {
+    color: 'cyan',
+    year: '2022.01.01',
+    title: '東京大神宮',
+    description: '初詣',
+  },
+  {
+    color: 'green',
+    year: '2022.01.29',
+    title: '陶芸教室',
+  },
+  {
+    color: 'pink',
+    year: '2022.04.22',
+    title: 'ひたちなか海浜公園',
+  },
+  {
+    color: 'amber',
+    year: '2022.05.07',
+    title: '初めてのTokyoTower　RED',
+  },
+  {
+    color: 'orange',
+    year: '2022.05.22',
+    title: 'Rakuten Stay at 河口湖'
+  },
+  {
+    color: 'orange',
+    year: '2022.06.11',
+    title: '鎌倉紫陽花'
+  },
+  {
+    color: 'orange',
+    year: '2022.06.18',
+    title: 'うんこ博士'
+  },
+  {
+    color: 'orange',
+    year: '2022.07.12',
+    title: '沖縄'
+  },
+  {
+    color: 'orange',
+    year: '2022.09.18',
+    title: '伊勢神宮'
+  },
+  {
+    color: 'orange',
+    year: '2022.10.27',
+    title: 'DisneyLand'
+  },
+  {
+    color: 'orange',
+    year: '2022.11.19',
+    title: '高尾山'
+  },
+  {
+    color: 'orange',
+    year: '2022.12.04',
+    title: 'Maroon5　LIVE'
+  },
+]
+
+const year2023 = [
+  {
+    color: 'cyan',
+    year: '2023.01.15',
+    title: '大分温泉',
+  },
+  {
+    color: 'green',
+    year: '2023.02.05',
+    title: '札幌&小樽',
+  },
+  {
+    color: 'green',
+    year: '2023.02.18',
+    title: '日常Cafe',
+  },
+  {
+    color: 'green',
+    year: '2023.05.14',
+    title: '金沢',
+  },
+  {
+    color: 'green',
+    year: '2023.07.21',
+    title: '青森',
+  },
+  {
+    color: 'green',
+    year: '2023.10.10',
+    title: '鹿児島',
+  },
+  {
+    color: 'green',
+    year: '2023.11.06',
+    title: 'Coldplay LIVE',
+  },
+  {
+    color: 'green',
+    year: '2023.12.04',
+    title: '旗の台から暮らし',
+  },
+]
+
+const year2024 = [
+  {
+    color: 'cyan',
+    year: '2024.01.31',
+    title: 'Ed Sheeran LIVE',
+  },
+  {
+    color: 'green',
+    year: '2024.07.03',
+    title: '府中花火大会',
+  },
+  {
+    color: 'green',
+    year: '2024.07.06',
+    title: '横瀬ホタル',
+  },
+]
+
+export const travelOptions = [
+  ...year2022,
+  ...year2023,
+  ...year2024,
+]
