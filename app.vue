41ff5f4bf986110cb8ad00ed17cca91727a2a90e
Jaycen <saitakusei@gmail.com>
2024-07-08 14:21:10 +0900

    Refactor timeline component

--- app.vue
+++ app.vue
@@ -1,32 +1,8 @@
 <template>
-  <v-timeline align="start">
-    <v-timeline-item
-      v-for="(option, i) in travelOptions"
-      :key="i"
-      :dot-color="randomColor()"
-      size="small"
-    >
-      <template v-slot:opposite>
-        <div
-          :class="`pt-1 headline font-weight-bold text-${randomColor()}`"
-          v-text="option.year"
-        ></div>
-      </template>
-      <div>
-        <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${randomColor()}`">
-          {{  option.title }}
-        </h2>
-        <div>
-          {{  option.description }}
-        </div>
-      </div>
-    </v-timeline-item>
-  </v-timeline>
+  <timeLine/>
 </template>
 
 <script setup>
-import {travelOptions} from './utils/options.ts'
-import {randomColor} from 'randomcolor'
+import timeLine from '../components/timeLine.vue'
 
-const years = travelOptions
 </script>
---
+++ components/timeLine.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-timeline align="start">
+    <v-timeline-item
+      v-for="(option, i) in travelOptions"
+      :key="i"
+      :dot-color="randomColor()"
+      size="small"
+    >
+      <template v-slot:opposite>
+        <div
+          :class="`pt-1 headline font-weight-bold text-${randomColor()}`"
+          v-text="option.year"
+        ></div>
+      </template>
+      <div>
+        <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${randomColor()}`">
+          {{  option.title }}
+        </h2>
+        <div>
+          {{  option.description }}
+        </div>
+      </div>
+    </v-timeline-item>
+  </v-timeline>
+</template>
+
+<script setup>
+import {travelOptions} from '../utils/options.ts'
+import {randomColor} from 'randomcolor'
+
+const years = travelOptions
+</script>
