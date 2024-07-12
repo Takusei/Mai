e5b9cbff8d3f03863b0412d75cad3eb8c4d94fa8
Jaycen <saitakusei@gmail.com>
2024-07-12 13:56:11 +0900

    Add typed toppage

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
-  <tab/>
+  <v-theme-provider class="pa-10 " theme="dark" with-background>
+    <Top/>
+  </v-theme-provider>
+  <!-- <tab/> -->
+
 </template>
 
 <script setup>
-import tab from '../components/tab.vue'
+// import tab from '../components/tab.vue's
+import Top from '../components/top.vue'
 
 </script>
---
+++ components/top.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container
+    class="bg-surface-variant full-screen"
+  >
+  <v-row
+      align="center"
+      no-gutters
+    > 
+      <Typed :options="options">
+        <h1 class="typing"></h1>
+      </Typed>
+    </v-row> 
+    <v-btn></v-btn>
+  </v-container>
+
+</template>
+
+<script setup lang="ts">
+import { Typed } from "@duskmoon/vue3-typed-js";
+import type { TypedOptions } from "@duskmoon/vue3-typed-js";
+
+const options: TypedOptions = {
+  strings: ["hello", "World", "This is vue3-typed-js"],
+  loop: false,
+  typeSpeed: 30,
+};
+</script>
+
+
+<style scoped>
+.full-screen {
+  height: 90vh;
+}
+</style>
