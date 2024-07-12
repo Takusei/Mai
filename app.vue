17d32f8bceb8f70797b6a12a9a87a5fa924c11c6
Jaycen <saitakusei@gmail.com>
2024-07-12 16:37:56 +0900

    Add dialog

--- app.vue
+++ app.vue
@@ -1,10 +1,18 @@
 <template>
   <v-theme-provider class="pa-10 " theme="dark" with-background>
-    <Top/>
+    <PopUp @closePopUp="closePopUp" />
+    <Top v-if="displayTop"/>
   </v-theme-provider>
 </template>
 
 <script setup>
 import Top from '../components/top.vue'
+import PopUp from '../components/popUp.vue'
+
+const displayTop = ref(false)
+
+const closePopUp = () => {
+  displayTop.value = true 
+}
 
 </script>
